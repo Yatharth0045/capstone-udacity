98f03e670742a80269e9e7bc33a696b2f36c22ff
Yatharth Sharma <yatharth0045@gmail.com>
2020-08-02 00:32:08 +0530

    ip updated

--- setup-controller.sh
+++ setup-controller.sh
@@ -6,7 +6,7 @@ PEM_FILE=~/Downloads/udacity-project.pem
 sudo chmod 400 ${PEM_FILE}
 
 ## Install python
-ssh -i ${PEM_FILE} ubuntu@${1:-34.201.205.43} "sudo apt update && sudo apt install -y python && sudo apt install -y tidy"
+ssh -i ${PEM_FILE} ubuntu@${1:-3.92.82.53} "sudo apt update && sudo apt install -y python && sudo apt install -y tidy"
 
 ## Run Ansible Role
 ansible-playbook -i roles/inventory.ini  ansible-main.yaml
