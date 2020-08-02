012acb345fa5b25734b31f679da1c25d8bb11e6f
Yatharth Sharma <yatharth0045@gmail.com>
2020-08-02 13:10:41 +0530

    updated index.html to fix linting

--- setup-controller.sh
+++ setup-controller.sh
@@ -6,7 +6,7 @@ PEM_FILE=~/Downloads/udacity-project.pem
 sudo chmod 400 ${PEM_FILE}
 
 ## Install python
-ssh -i ${PEM_FILE} ubuntu@${1:-3.92.82.53} "sudo apt update && sudo apt install -y python && sudo apt install -y tidy"
+ssh -i ${PEM_FILE} ubuntu@${1:-54.167.115.229} "sudo apt update && sudo apt install -y python && sudo apt install -y tidy"
 
 ## Run Ansible Role
 ansible-playbook -i roles/inventory.ini  ansible-main.yaml
